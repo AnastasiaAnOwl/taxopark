888c690c27900aa96edcd750d259c70e04f4b5d5
AnOwl <a.v.kunatava@gmail.com>
2018-12-10 20:21:56 +0300

    CarModel enum added

--- src/by/owl/coursesproject/taxopark/CarFactory.java
+++ src/by/owl/coursesproject/taxopark/CarFactory.java
@@ -6,18 +6,18 @@ import by.owl.coursesproject.taxopark.exception.UnknownModelException;
 
 public class CarFactory {
 
-	public static Car create(String model) {
+	public static Car create(CarModel model) {
 		switch (model) {
-		case "Kia Ceed's":
+		case KIA_CEEDS:
 			return new Car("Kia Ceed's", new Gasoline(), 6.8, 200, 6_000);
-		case "Toyota Corolla":
+		case TOYOTA_COROLLA:
 			return new Car("Toyota Corolla", new Diesel(), 6.6, 270, 10_000);
-		case "Opel Zafira":
+		case OPEL_ZAFIRA:
 			return new Car("Opel Zafira", new Gasoline(), 6.6, 180, 8_000);
-		case "BMW X5":
+		case BMW_X5:
 			return new Car("BMW X5", new Gasoline(), 7, 300, 15_000);
 		default:
-			throw new UnknownModelException(model);
+			throw new UnknownModelException(model.getName());
 		}
 
 	}
---
+++ src/by/owl/coursesproject/taxopark/CarModel.java
@@ -0,0 +1,17 @@
+package by.owl.coursesproject.taxopark;
+
+public enum CarModel {
+
+	KIA_CEEDS("Kia Ceed's"), TOYOTA_COROLLA("Toyota Corolla"), OPEL_ZAFIRA("Opel Zafira"), BMW_X5("BMW X5");
+
+	private String name;
+
+	CarModel(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
