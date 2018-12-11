82528b24889ef57beda863a6ad6402fd2bfb736e
AnOwl <a.v.kunatava@gmail.com>
2018-12-11 17:19:24 +0300

    check if reading line is empty added.

--- src/by/owl/coursesproject/taxopark/Application.java
+++ src/by/owl/coursesproject/taxopark/Application.java
@@ -34,44 +34,52 @@ public class Application {
 			System.out.println("3. Exit");
 			System.out.print("[1, 2, 3] >");
 			String line = br.readLine();
-			switch (line.charAt(0)) {
-			case '1':
-				INNER_LOOP: do {
-					System.out.println("How would you like to sort them?");
-					System.out.println("1. By price");
-					System.out.println("2. By maximum speed");
-					System.out.println("3. By fuel consumption");
-					System.out.println("4. Get back to menu");
-					System.out.println("[1, 2, 3, 4] >");
-					line = br.readLine();
-					switch (line.charAt(0)) {
-					case '1':
-						carPark.sort((Car car0, Car car1) -> car0.getPrice() - car1.getPrice());
-						carPark.getCars().forEach(System.out::println);
-						break;
-					case '2':
-						carPark.sort((Car car0, Car car1) -> car0.getMaxSpeed() - car1.getMaxSpeed());
-						carPark.getCars().forEach(System.out::println);
-						break;
-					case '3':
-						carPark.sort((Car car0,
-								Car car1) -> (int) (car0.getFuelConsumption() * 100 - car1.getFuelConsumption() * 100));
-						carPark.getCars().forEach(System.out::println);
-						break;
+			if (line.isEmpty()) {
+				continue OUTER_LOOP;
+			} else {
+				switch (line.charAt(0)) {
+				case '1':
+					INNER_LOOP: do {
+						System.out.println("How would you like to sort them?");
+						System.out.println("1. By price");
+						System.out.println("2. By maximum speed");
+						System.out.println("3. By fuel consumption");
+						System.out.println("4. Get back to menu");
+						System.out.println("[1, 2, 3, 4] >");
+						line = br.readLine();
+						if (line.isEmpty()) {
+							continue INNER_LOOP;
+						} else {
+							switch (line.charAt(0)) {
+							case '1':
+								carPark.sort((Car car0, Car car1) -> car0.getPrice() - car1.getPrice());
+								carPark.getCars().forEach(System.out::println);
+								break;
+							case '2':
+								carPark.sort((Car car0, Car car1) -> car0.getMaxSpeed() - car1.getMaxSpeed());
+								carPark.getCars().forEach(System.out::println);
+								break;
+							case '3':
+								carPark.sort((Car car0, Car car1) -> (int) (car0.getFuelConsumption() * 100
+										- car1.getFuelConsumption() * 100));
+								carPark.getCars().forEach(System.out::println);
+								break;
 
-					case '4':
-						break INNER_LOOP;
-					}
-				} while (true);
-				break;
+							case '4':
+								break INNER_LOOP;
+							}
+						}
+					} while (true);
+					break;
 
-			case '2':
-				System.out.println();
-				System.out.println("The total cost of taxi station is " + moneyString);
-				System.out.println();
-				break;
-			case '3':
-				break OUTER_LOOP;
+				case '2':
+					System.out.println();
+					System.out.println("The total cost of taxi station is " + moneyString);
+					System.out.println();
+					break;
+				case '3':
+					break OUTER_LOOP;
+				}
 			}
 		} while (true);
 
